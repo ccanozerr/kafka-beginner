c12bff990056d5ae5caa8e0264492c6ead5e0cd1
ccanozerr <ccanozerr@gmail.com>
2021-06-09 15:13:24 +0300

    add: producers with keys

---
+++ src/main/java/com/ccanozerr/kafka/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,68 @@
+package com.ccanozerr.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoKeys {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create thee producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++){
+
+            // create producer record
+            String topic = "first_topic";
+            String value = i + ". Value";
+            String key = i + ". Key";
+
+            ProducerRecord<String, String>  record =
+                    new ProducerRecord<>(topic, key, value);
+
+            logger.info("Key: " + key); // log the key
+            /*
+                If we send message with same key will always goes to same partition
+             */
+
+            // send data - asynchronous
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if(e == null){
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get(); // block the .send() to make it synchronous - don't do this in production!
+        }
+
+        // flush data
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+    }
+}
